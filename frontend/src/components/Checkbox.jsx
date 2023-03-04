ef39790b4393395896a62ededa4a202ca921404a
derp000 <84947701+derp000@users.noreply.github.com>
2023-03-04 18:18:58 -0500

    Create checkbox component

--- frontend/src/components/Checkbox.jsx
+++ frontend/src/components/Checkbox.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Checkbox = ({ text }) => {
   return (
     <div className="md:inline-flex md:items-center">
-      <input type="checkbox" checked className="checkbox checkbox-lg mr-5" />
-      <p className="text-left py-6 lg:text-2xl text-2xl md:max-w-md lg:leading-loose leading-loose">
+      <input type="checkbox" checked className="checkbox checkbox-lg mr-10" />
+      <p className="text-left py-6 lg:text-2xl text-2xl md:max-w-5xl lg:leading-loose leading-loose">
         {text}
       </p>
     </div>
--- frontend/src/components/Checklist.jsx
+++ frontend/src/components/Checklist.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import Checkbox from "./Checkbox";
 
+const criteria = [
+  "In an effort to make astronaut training and space more accessible, Helios accepts astronaut applications from any UN recognized country or territory!",
+  "Pass ACTFL English fluency exam",
+  "(Suggested) A doctorate degree from an accredited university or equivalent experience in natural sciences, medicine, or engineering*",
+  "Teaching K-12 is considered equivalent for an astronaut candidate; educators are encouraged to apply!",
+];
+
 const Checklist = () => {
   return (
     <div className="hero h-fit bg-base-200 px-2">
       <div className={"hero-content"}>
         <div className="max-w-5xl">
-          <h1 className="md:text-7xl text-5xl font-bold mt-2 text-center">
+          <h6 className="md:text-2xl text-xl font-bold mt-10 text-center">
             What does it take to get started?
+          </h6>
+          <h1 className="md:text-7xl text-5xl font-bold mt-2 text-center">
+            Eligibility Requirements
           </h1>
-          <div className="flex flex-col md:gap-10 md:mt-5 md:items-start">
-            <Checkbox text={"hi world"} />
-            <Checkbox text={"check2"} />
+          <div className="flex flex-col md:gap-2 md:mt-5 md:items-start">
+            {criteria.map((crit, i) => (
+              <Checkbox text={crit} key={i} />
+            ))}
           </div>
         </div>
       </div>
