322e661524d61a5ab3b83687e8c6a5abf87142e6
derp000 <84947701+derp000@users.noreply.github.com>
2023-03-13 00:15:02 -0400

    Create ReviewCard

--- frontend/src/containers/Home.jsx
+++ frontend/src/containers/Home.jsx
@@ -4,13 +4,12 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import { LandingPage, AboutUs, Pricing, Legal } from "../components";
 import Footer from "../components/Footer";
-import MarsGlobe from "../components/MarsGlobe";
-import MarsGlobeArc from "../components/MarsGlobeArc";
-import TestResize from "../components/TestResize";
+import ReviewCard from "../components/ReviewCard";
 import NavbarTest from "../components/NavbarTest";
 import Eligibility from "../components/Eligibility";
 import Logistics from "../components/Logistics";
 import Attractions from "../components/Attractions";
+import GlobeArcPoints from "../components/GlobeArcPoints";
 
 const categories = [
   { name: "About", to: "AboutUs" },
@@ -36,8 +35,8 @@ const Home = () => {
         <Route path="/Pricing" element={<Pricing />} />
 
         <Route path="/Legal" element={<Legal />} />
-        <Route path="/Testing4" element={<MarsGlobeArc />} />
-        <Route path="/Testing2" element={<TestResize />} />
+        <Route path="/Testing4" element={<GlobeArcPoints />} />
+        <Route path="/Testing2" element={<ReviewCard />} />
         <Route path="/Testing3" element={<NavbarTest />} />
         <Route path="/Eligibility" element={<Eligibility />} />
         {/* Outreach (promotion),  */}
