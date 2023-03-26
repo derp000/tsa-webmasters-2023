6e9eea42f612a7f4be173a99a298ad2618002a17
derp000 <84947701+derp000@users.noreply.github.com>
2023-03-26 00:04:47 -0400

    Fix Checkbox by removing md css query

--- frontend/src/components/Checkbox.jsx
+++ frontend/src/components/Checkbox.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Checkbox = ({ textElem }) => {
   return (
-    <div className="md:inline-flex md:items-center">
+    <div className="inline-flex items-center">
       <input type="checkbox" checked className="checkbox checkbox-lg mr-10" />
       <div className="text-left py-6 lg:text-2xl text-2xl md:max-w-5xl lg:leading-loose leading-loose">
         {textElem}
