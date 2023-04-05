e418f594216f70a2a184098f63ed84e903d35d34
derp000 <84947701+derp000@users.noreply.github.com>
2023-04-04 20:30:26 -0400

    Modify tailwind config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   mode: "jit",
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        helios: {
+          primary: "#fde68a",
+          secondary: "#93c5fd",
+          accent: "#a78bfa",
+          neutral: "#191D24",
+          "base-100": "#374151",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
   },
   plugins: [require("@headlessui/tailwindcss"), require("daisyui")],
 };
